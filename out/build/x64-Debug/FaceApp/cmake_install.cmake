7bafb9a437312335c6c105af57e6c33edb1db684
Nikita Korneich <korneich.nik@gmail.com>
2022-03-17 18:07:31 +0700

    After Daniil Destroy recovery AUE

--- out/build/x64-Debug/FaceApp/cmake_install.cmake
+++ out/build/x64-Debug/FaceApp/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/korne/Desktop/projects on cpp/FaceApp/FaceApp
+# Install script for directory: C:/Users/korne/Desktop/projects on cpp/FaceApp-CPP/FaceApp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/korne/Desktop/projects on cpp/FaceApp/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/korne/Desktop/projects on cpp/FaceApp-CPP/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
--- out/build/x64-Debug/cmake_install.cmake
+++ out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/korne/Desktop/projects on cpp/FaceApp
+# Install script for directory: C:/Users/korne/Desktop/projects on cpp/FaceApp-CPP
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/korne/Desktop/projects on cpp/FaceApp/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/korne/Desktop/projects on cpp/FaceApp-CPP/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/korne/Desktop/projects on cpp/FaceApp/out/build/x64-Debug/FaceApp/cmake_install.cmake")
+  include("C:/Users/korne/Desktop/projects on cpp/FaceApp-CPP/out/build/x64-Debug/FaceApp/cmake_install.cmake")
 
 endif()
 
@@ -46,5 +46,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/korne/Desktop/projects on cpp/FaceApp/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/korne/Desktop/projects on cpp/FaceApp-CPP/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
